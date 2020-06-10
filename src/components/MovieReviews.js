bc7b35312001bf2007e77b7907e793b9d56f3407
ctdupuis <ctdupuis7@gmail.com>
2020-06-09 23:40:13 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+  
+import React from 'react';
+
+const MovieReviews = props => { 
+     const { reviews } = props
+
+     const reviewList = reviews.map((review, index) => {
+          return (
+               <li key={index}>
+                    <div className="review">
+                         <span>Display Title</span>: {review.display_title}
+                         <span>Headline</span>: {review.headline}
+                    </div>
+               </li>
+          )
+     })
+     return (
+          <ol className="review-list">
+               {reviewList}
+          </ol>
+     );
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,38 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        fetch(URL.concat(this.state.searchTerm))
+        .then(r => r.json())
+        .then(d => this.setState({ reviews: d.results }))
+        event.preventDefault()
+        event.target.reset()
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ searchTerm: event.target.value })
+    }
+
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='search-input'>Search Reviews</label>
+                    <input type="text" id="search-input" onChange={this.handleInputChange} />
+                    <button type="submit">Search</button>
+                </form>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+
+}
