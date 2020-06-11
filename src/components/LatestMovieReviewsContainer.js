d18657d5b2b361d1cffceb19e332c15e002c4750
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-06-11 18:44:14 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'LC7uMqLYGB7agdxzwxn7AheA4JaCvZkL';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+  + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(reviewsData => this.setState({ reviews: reviewsData.results }))
+  }
+
+  render() {
+    return (
+      <div className='latest-movie-reviews'>
+        <h2>Latest Movie Reviews</h2>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,45 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'LC7uMqLYGB7agdxzwxn7AheA4JaCvZkL';
+const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+  + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchTerm: '',
+      reviews: []
+    }
+  }
+  handleSearchInputChange = event => {
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    fetch(BASE_URL.concat(this.state.searchTerm))
+      .then(resp => resp.json())
+      .then(reviewsData => this.setState({ reviews: reviewsData.results }));
+  }
+
+  render() {
+    return (
+      <div className='searchable-movie-reviews'>
+        <form onSubmit={this.handleSubmit}>
+          <label>Search Movie Reviews</label>
+          <input
+            id='search-input'
+            type='text'
+            onChange={this.handleSearchInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
